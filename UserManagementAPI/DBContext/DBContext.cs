c54f5668773924f7335e61b800635c994c5f4b7e
mert oğuz <mertoguz@hotmail.com.tr>
2021-03-09 23:31:55 +0300

    code cleanup

--- UserManagementAPI/DBContext/DBContext.cs
+++ UserManagementAPI/DBContext/DBContext.cs
@@ -13,33 +13,6 @@ namespace UserManagementAPI.DBContext
         {
 
         }
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-        //    modelBuilder.Entity<User>().HasData(
-        //        new User
-        //        {
-        //            TCKN = "12345678912",
-        //            Name = "Mert",
-        //            Surname = "OĞUZ",
-        //            Address = "mertoguz Adresi",
-        //            LastModifiedDate = DateTime.Now,
-        //            Birthday = Convert.ToDateTime("08/12/1995"),
-        //            CreateTime = DateTime.Now,
-
-        //        },
-        //        new User
-        //        {
-        //            TCKN = "12345678913",
-        //            Name = "123Mert",
-        //            Surname = "OĞUZ",
-        //            Address = "1234mertoguz Adresi",
-        //            LastModifiedDate = DateTime.Now,
-        //            Birthday = Convert.ToDateTime("09/12/1995"),
-        //            CreateTime = DateTime.Now,
-        //        }
-        //        );
-        //}
 
         public DbSet<User> Users { get; set; }
     }
